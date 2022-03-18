3b00edd3b9c1b60ea875cb184533ace4bc196580
guofeng <guofeng@elestorm.com>
2022-03-18 17:46:16 +0800

    update gitbook

--- commit.sh
+++ commit.sh
@@ -1,2 +1,18 @@
-echo '执行 gitbook build 命令'
-gitbook build .
+echo '生成目录'
+python ../python-utils/main.py
+
+echo '构建gitbook'
+gitbook build
+
+echo '切换到gf530142771.github.io目录 切换分支到 gh-pages 删除所有数据'
+cd ..
+cd gf530142771.github.io
+git checkout gh-pages
+rm -r *
+echo '复制新的_book信息'
+cp -r ../markdown-notes/_book/* .
+echo '更新分支 并上传'
+git add .
+git commit -m "update gitbook"
+git push origin gh-pages
+
